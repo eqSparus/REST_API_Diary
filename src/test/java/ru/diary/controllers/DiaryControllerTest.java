c2f8e1ffa1299ee6bae912123db7eb5214da9562
Федорышин Константин <kot-1693@mail.ru>
2021-11-29 09:08:18 +0600

    new

---
+++ src/test/java/ru/diary/controllers/DiaryControllerTest.java
@@ -0,0 +1,61 @@
+package ru.diary.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import ru.diary.config.AppConfigTest;
+import ru.diary.configurations.security.jwt.TokenCreator;
+import ru.diary.models.form.DiaryForm;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppConfigTest.class)
+@WebAppConfiguration
+@Sql(value = {"/create_user_before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/close_bd.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(value = {"/create_user_after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+class DiaryControllerTest {
+
+    private WebApplicationContext wac;
+    private TokenCreator creator;
+    private MockMvc mvc;
+
+    @Autowired
+    public DiaryControllerTest(WebApplicationContext wac, TokenCreator creator) {
+        this.wac = wac;
+        this.creator = creator;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    void createDiary() throws Exception {
+
+        var mapper = new ObjectMapper();
+        var diary = new DiaryForm("Diary");
+
+        mvc.perform(post("/crud/diary/create")
+                        .content(mapper.writeValueAsString(diary)).contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .header("Authorization", creator.createToken("sparus-1693@yandex.ru"))
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
+    }
+
+}
---
+++ src/test/resources/close_bd.sql
@@ -0,0 +1,3 @@
+DELETE FROM records;
+DELETE FROM labels;
+DELETE FROM diaries;
